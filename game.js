fadd0806214fae5cef6416edf155d414d40f177a
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-04-03 11:49:26 +0530

    implemented the functions using switch case

--- game.js
+++ game.js
@@ -1,64 +1,66 @@
-function getComputerChoice(){
-    let options = ["Rock", "Paper", "Scissors"];
-    const index = Math.floor(Math.random()*options.length);
-    return options[index];
-}
-function playRound(playerSelection,computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-        return("Tie");
-    }
-    else if (playerSelection.toLowerCase() === "rock"){
-        if (computerSelection.toLowerCase() === "scissors"){
-            return("Player");
-        }
-        else {
-            return("Computer");
-        }
-    }
-    else if (playerSelection.toLowerCase() === "paper"){
-        if (computerSelection.toLowerCase() === "rock"){
-            return("Player");
-        }
-        else {
-            return("Computer");
-        }
-    }
-    else if (playerSelection.toLowerCase() === "scissors"){
-        if (computerSelection.toLowerCase() === "paper"){
-            return("Player");
-        }
-        else {
-            return("Computer");
-        }
-    }
-    else {
-        return("Invalid choice!");
-    }
-}
-function printResult(result,playerSelection,computerSelection) {
-    if (result === "Player"){
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-    }
-    else if (result === "Computer"){
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-    }
-    else {
-        console.log("Its a Tie!");
+let compPoints = 0;
+let playerPoints = 0;
+let options = ["Rock", "Paper", "Scissors"];
+
+getComputerChoice = () => options[Math.floor(Math.random() * options.length)]
+
+playRound = (playerSelection,computerSelection) => {
+    switch(playerSelection.toLowerCase()) {
+        case 'rock':
+            switch(computerSelection.toLowerCase()) {
+                case 'paper':
+                    return("Computer");
+                case 'scissors':
+                    return("Player");
+                default:
+                    return("Tie");
+            }
+        case 'paper':
+            switch(computerSelection.toLowerCase()) {
+                case 'scissors':
+                    return("Computer");
+                case 'rock':
+                    return("Player");
+                default:
+                    return("Tie");
+            }
+        case 'scissors':
+            switch(computerSelection.toLowerCase()) {
+                case 'rock':
+                    return("Computer");
+                case 'paper':
+                    return("Player");
+                default:
+                    return("Tie");
+            }
+        default:
+            return("Invalid Choice");
     }
 }
-function game(){
-    let compPoints = 0;
-    let playerPoints = 0;
+
+printResult = (result,playerSelection,computerSelection) => {
+    switch(result) {
+        case "Player":
+            playerPoints += 1;
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            break;
+        case "Computer":
+            compPoints += 1;
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            break;
+        case "Tie":
+            console.log("Its a Tie!");
+            break;
+        default:
+            console.log("Invalid Choice");
+    }
+}
+
+game = () => {
     for(let i = 0; i <5;i++){
         let playerSelection = prompt("Enter your choice: ");
         let computerSelection = getComputerChoice();
         let result = playRound(playerSelection,computerSelection);
-        if (result === "Player"){
-            playerPoints++;
-        }
-        else if (result === "Computer"){
-            compPoints++;
-        }
         printResult(result,playerSelection,computerSelection);
     }
     if (playerPoints > compPoints){
@@ -71,4 +73,5 @@ function game(){
         alert("It's a Tie!");
     }
 }
+
 game();
